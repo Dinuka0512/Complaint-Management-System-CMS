8d5fa4cdfc05637bd2daca6e12aaa4a9c975e102
Dinuka Lakmal <Dinuka0512@gmail.com>
2025-06-17 00:08:00 +0530

    added the favicon

---
+++ src/main/java/com/example/jspcmsfinal/LoginServlet.java
@@ -0,0 +1,11 @@
+package com.example.jspcmsfinal;
+
+import java.io.*;
+
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+
+}
