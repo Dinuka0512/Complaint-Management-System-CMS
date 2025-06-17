cbb5be984502af8f7b063bfe6ad6361fee866a2a
Dinuka Lakmal <Dinuka0512@gmail.com>
2025-06-18 00:11:38 +0530

    changed the database

--- src/main/resources/database/db.sql
+++ src/main/resources/database/db.sql
@@ -2,7 +2,7 @@ CREATE DATABASE CMS;
 USE CMS;
 
 CREATE TABLE User (
-                      uId VARCHAR(255),
+                      uId VARCHAR(255) PRIMARY KEY ,
                       name VARCHAR(255),
                       email VARCHAR(255),
                       password VARCHAR(255),
@@ -10,18 +10,20 @@ CREATE TABLE User (
 );
 
 CREATE TABLE Complain (
-                          complainId VARCHAR(255),
+                          complainId VARCHAR(255) PRIMARY KEY ,
                           uId VARCHAR(255),
                           subject VARCHAR(255),
                           message VARCHAR(255),
                           status VARCHAR(255),
+                          date DATE,
                           FOREIGN KEY (uId) REFERENCES User(uId) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE Answer (
-                        ansId VARCHAR(255),
+                        ansId VARCHAR(255) PRIMARY KEY ,
                         complainId VARCHAR(255),
                         subject VARCHAR(255),
                         message VARCHAR(255),
+                        date DATE,
                         FOREIGN KEY (complainId) REFERENCES Complain(complainId) ON DELETE CASCADE ON UPDATE CASCADE
 );
